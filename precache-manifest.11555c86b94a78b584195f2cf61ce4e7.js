42b2b55f5a3a73540138e9144a474af2febbc4fa
Alina Karpenkova <karpenkova.alina12@gmail.com>
2020-10-12 11:59:58 +0300

    Updates

---
+++ precache-manifest.11555c86b94a78b584195f2cf61ce4e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01499682118408fd1a4b0428d57fd414",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "025c4edae1e1aecf0a8d",
+    "url": "/react-tetris/static/css/main.6b7ee09b.chunk.css"
+  },
+  {
+    "revision": "205ed7c73228de915bd6",
+    "url": "/react-tetris/static/js/2.c4b5ed7a.chunk.js"
+  },
+  {
+    "revision": "025c4edae1e1aecf0a8d",
+    "url": "/react-tetris/static/js/main.41ab5aff.chunk.js"
+  },
+  {
+    "revision": "9f8e8f985e23fe474e69",
+    "url": "/react-tetris/static/js/runtime~main.9a7ed037.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
